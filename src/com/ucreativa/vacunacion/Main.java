14badbcd485630785ae6caa1446d31c1c80cc2a8
Tattiana-alfaro <t$77Sto30yru>
2021-06-03 20:42:26 -0600

    Trabajo en clase #8: modifique el main para que capture los campos de la consola

--- src/com/ucreativa/vacunacion/Main.java
+++ src/com/ucreativa/vacunacion/Main.java
@@ -4,32 +4,61 @@ import com.ucreativa.vacunacion.entities.BitacoraVacuna;
 import com.ucreativa.vacunacion.entities.Familiar;
 import com.ucreativa.vacunacion.entities.Persona;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
+
+import static java.lang.Integer.parseInt;
 
 public class Main {
 
     public static void main(String[] args) {
-        Familiar pareja = new Familiar("Adrian", "AAA-123",
-                33, false, "Pareja");
+        Scanner in = new Scanner(System.in);
+        List<BitacoraVacuna> db = new ArrayList<>();
+        String nombre, cedula, edad, riesgo, isAmigo, relacion, facebook, parentesco, marca, print;
+        Persona persona;
 
-        Familiar padre = new Familiar("Allan", "BBB-123",
-                51, true, "Padre");
+        while (true) {
+            System.out.println("Nombre: ");
+            nombre = in.nextLine();
 
-        Amigo compaTrabajo = new Amigo("Oscar", "CCC-456",
-                38, false, "Trabajo", "No se");
+            System.out.println("Cedula: ");
+            cedula = in.nextLine();
 
-        List<Persona> listaPersonas = new ArrayList<>();
-        listaPersonas.add(pareja);
-        listaPersonas.add(padre);
-        listaPersonas.add(compaTrabajo);
+            System.out.println("Edad: ");
+            edad = in.nextLine();
 
-        List<BitacoraVacuna> bitacora = new ArrayList<>();
-        bitacora.add(new BitacoraVacuna(pareja, "Pfizer", new Date()));
-        bitacora.add(new BitacoraVacuna(padre, "Pfizer", new Date()));
-        bitacora.add(new BitacoraVacuna(compaTrabajo, "Pfizer", new Date()));
+            System.out.println("Riesgo(S/N): ");
+            riesgo = in.nextLine();
+
+            System.out.println("Amigo(A)/Familiar (F): ");
+            isAmigo = in.nextLine();
+
+            if (isAmigo.toUpperCase(Locale.ROOT) == "A") {
+                System.out.println("Relacion: ");
+                relacion = in.nextLine();
+
+                System.out.println("Facebook: ");
+                facebook = in.nextLine();
+                persona = new Amigo(nombre, cedula, parseInt(edad), riesgo.equals("S"), relacion, facebook);
+            } else {
+                System.out.println("Parentesco: ");
+                parentesco = in.nextLine();
+                persona = new Familiar(nombre, cedula, parseInt(edad), riesgo.equals("S"), parentesco);
+            }
+
+            System.out.println("Marca: ");
+            marca = in.nextLine();
 
 
+            db.add(new BitacoraVacuna(persona, marca, new Date()));
+            System.out.println("Quiere Imprimir Lista(S)? ");
+            print = in.nextLine();
+            if (print.equals("S")){
+                for (BitacoraVacuna item : db)
+                {
+                    System.out.println(item.getPersona().getNombre() + " vacunado el " + item.getFecha());
+
+                }
+            }
+        }
     }
 }
