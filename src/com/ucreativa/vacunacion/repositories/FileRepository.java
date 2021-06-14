6b73f3b5f6b2add3a242316391eb3fd38972ed97
Tattiana-alfaro <t$77Sto30yru>
2021-06-14 17:47:59 -0600

    Add BufferReader class

--- src/com/ucreativa/vacunacion/repositories/FileRepository.java
+++ src/com/ucreativa/vacunacion/repositories/FileRepository.java
@@ -2,14 +2,14 @@ package com.ucreativa.vacunacion.repositories;
 
 import com.ucreativa.vacunacion.entities.Persona;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class FileRepository implements  Repository{
+    private final String FILE_PATH = "db.txt";
 
 
     @Override
@@ -17,7 +17,7 @@ public class FileRepository implements  Repository{
         SimpleDateFormat format = new SimpleDateFormat("ss-MM-yyy hh:mm:ss");
         String text = persona.getNombre() + " - " + marca + " - " + format.format(fecha) + "\n";
         try {
-            BufferedWriter writer = new BufferedWriter(new FileWriter("db.txt",true ));
+            BufferedWriter writer = new BufferedWriter(new FileWriter(FILE_PATH,true ));
             writer.append(text);
             writer.close();
 
@@ -28,6 +28,12 @@ public class FileRepository implements  Repository{
 
     @Override
     public List<String> get() {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(FILE_PATH));
+            return reader.lines().collect(Collectors.toList());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 }
